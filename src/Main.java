45e7f47227eb040f4165360a636e701e2c29a88c
Marco-Poelsma <marco.poelsma@gracia.lasalle.cat>
2024-10-04 11:25:09 +0200

    Exercici 4

---
+++ src/Main.java
@@ -0,0 +1,36 @@
+/*Exercici 4*/
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner (System.in);
+        System.out.println("Bon dia! Introdueix el nombre de nois a la classe.");
+        boolean valid = true;
+        double nNois;
+        double nNoies;
+        do {
+            nNois = scan.nextInt();
+            if (!scan.hasNextInt() || nNois < 0) {
+                System.out.println("Introdueix un nombre enter positiu.");
+                valid = false;
+
+            }
+        } while (!valid);
+        System.out.println("Introdueix el nombre de noies a la classe.");
+        do {
+            nNoies = scan.nextInt();
+            if (!scan.hasNextInt() || nNoies < 0) {
+                System.out.println("Introdueix un nombre enter positiu.");
+                valid = false;
+
+            }
+        } while (!valid);
+        double nTotal =  nNois+nNoies;
+        double percentNois = nNois/nTotal;
+        double percentNoies = nNoies/nTotal;
+        System.out.println("El percentatge de nois a la classe és de: " + percentNois + "%");
+        System.out.println("El percentatge de noies a la classe és de: " + percentNoies + "%");
+
+
+
+    }
+}
