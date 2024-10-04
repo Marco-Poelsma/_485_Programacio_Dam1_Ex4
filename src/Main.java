de7d24bd140e97514d9eb7498bd1a153cfbda8e7
Marco-Poelsma <marco.poelsma@gracia.lasalle.cat>
2024-10-04 13:00:28 +0200

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,36 +1,41 @@
 /*Exercici 4*/
 import java.util.Scanner;
+import java.lang.String;
 public class Main {
     public static void main(String[] args) {
         Scanner scan = new Scanner (System.in);
         System.out.println("Bon dia! Introdueix el nombre de nois a la classe.");
+        int nNois = 0;
+        int nNoies = 0;
         boolean valid = true;
-        int nNois;
-        int nNoies;
-        do {
-            if (!scan.hasNextInt() || nNois < 0) {
-                System.out.println("Introdueix un nombre enter positiu.");
-                valid = false;
+        do{
+            if (!valid){
+                System.out.println("Si us plau, introdueix un nombre enter positiu.");
             }
-            else{
-                valid=true;
+            while(!scan.hasNextInt()){
+                scan.nextLine();
+                System.out.println("Si us plau, introdueix un nombre enter positiu.");
             }
             nNois = scan.nextInt();
-        } while (!valid);
+            valid = nNois >= 0;
+        }while (!valid);
         System.out.println("Introdueix el nombre de noies a la classe.");
-        do {
-            if (!scan.hasNextInt() || nNoies < 0) {
-                System.out.println("Introdueix un nombre enter positiu.");
-                valid = false;
+        do{
+            if (!valid){
+                System.out.println("Si us plau, introdueix un nombre enter positiu.");
             }
-            else{
-                valid=true;
+            while(!scan.hasNextInt()){
+                System.out.println("Si us plau, introdueix un nombre enter positiu.");
+                String clearCache = scan.nextLine();
+                scan.nextLine();
+
             }
             nNoies = scan.nextInt();
-        } while (!valid);
+            valid = nNoies >= 0;
+        }while (!valid);
         double nTotal =  nNois+nNoies;
-        double percentNois = nNois/nTotal;
-        double percentNoies = nNoies/nTotal;
+        double percentNois = (nNois/nTotal)*100;
+        double percentNoies = (nNoies/nTotal)*100;
         System.out.println("El percentatge de nois a la classe és de: " + percentNois + "%");
         System.out.println("El percentatge de noies a la classe és de: " + percentNoies + "%");
 
